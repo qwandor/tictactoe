d4ece68d0c9d2c8f8ef460240b6a8e5c74a88916
Andrew Walbran <qwandor@gmail.com>
2014-12-31 10:37:33 +0000

    Method to find guaranteed winner from a state

--- src/nz/q/geek/tictactoe/TicTacToe.java
+++ src/nz/q/geek/tictactoe/TicTacToe.java
@@ -7,7 +7,8 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		GameState initialState = new GameState(BLUE, new Player[SIZE][SIZE][SIZE]);
-		explore(initialState);
+		//explore(initialState);
+		System.out.println("From blank board, " + winner(initialState) + " wins.");
 	}
 
 	public static void explore(GameState state) {
@@ -30,6 +31,45 @@ public class TicTacToe {
 					}
 				}
 			}
+		}
+	}
+
+	/**
+	 * Check whether either player has a guaranteed win from the given state.
+	 * @return the player who can win no matter what the other player does, or null if neither is guaranteed.
+	 */
+	public static Player winner(GameState state) {
+		Player alreadyWon = state.winner();
+		if (alreadyWon != null) {
+			return alreadyWon;
+		} else if (state.full()) {
+			return null;
+		}
+
+		// Try all possible moves for the current player
+		boolean stalematePossible = false;
+		for (int x = 0; x < SIZE; ++x) {
+			for (int y = 0; y < SIZE; ++y) {
+				for (int z = 0; z < SIZE; ++z) {
+					if (state.board[x][y][z] == null) {
+						GameState newState = state.makeMove(x, y, z);
+						Player newWinner = winner(newState);
+						if (newWinner == state.nextPlayer) {
+							// If the current player has a guaranteed win from the new state, they have a guaranteed win from this state.
+							return newWinner;
+						} else if (newWinner == null) {
+							// We would rather a stalemate than the opposing player winning.
+							stalematePossible = true;
+						}
+					}
+				}
+			}
+		}
+		// No way for current player to win. Either stalemate, or other player wins.
+		if (stalematePossible) {
+			return null;
+		} else {
+			return state.nextPlayer.opposite();
 		}
 	}
 }
