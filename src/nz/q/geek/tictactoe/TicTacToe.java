9ea91e992e5f5c14e94604700eeb038f752b6b8d
Andrew Walbran <qwandor@gmail.com>
2014-12-31 12:09:49 +0000

    Cache results already known.

--- src/nz/q/geek/tictactoe/TicTacToe.java
+++ src/nz/q/geek/tictactoe/TicTacToe.java
@@ -2,16 +2,24 @@ package nz.q.geek.tictactoe;
 
 import static nz.q.geek.tictactoe.Player.BLUE;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TicTacToe {
 	private static final int SIZE = 3;
+	private final Map<GameState, Player> knownWinners = new HashMap<>();
 
 	public static void main(String[] args) {
+		new TicTacToe().start();
+	}
+
+	public void start() {
 		GameState initialState = new GameState(BLUE, new Player[SIZE][SIZE][SIZE]);
 		//explore(initialState);
 		System.out.println("From blank board of size " + SIZE + ", " + BLUE + " starting, " + cachedWinner(initialState, 0) + " wins.");
 	}
 
-	public static void explore(GameState state) {
+	public void explore(GameState state) {
 		Player winner = state.winner();
 		if (winner != null) {
 			System.out.println(winner + " wins");
@@ -38,7 +46,7 @@ public class TicTacToe {
 	 * Check whether either player has a guaranteed win from the given state.
 	 * @return the player who can win no matter what the other player does, or null if neither is guaranteed.
 	 */
-	public static Player winner(GameState state) {
+	public Player winner(GameState state, int depth) {
 		Player alreadyWon = state.winner();
 		if (alreadyWon != null) {
 			return alreadyWon;
@@ -53,7 +61,7 @@ public class TicTacToe {
 				for (int z = 0; z < SIZE; ++z) {
 					if (state.board[x][y][z] == null) {
 						GameState newState = state.makeMove(x, y, z);
-						Player newWinner = winner(newState);
+						Player newWinner = cachedWinner(newState, depth + 1);
 						if (newWinner == state.nextPlayer) {
 							// If the current player has a guaranteed win from the new state, they have a guaranteed win from this state.
 							return newWinner;
@@ -71,5 +79,18 @@ public class TicTacToe {
 		} else {
 			return state.nextPlayer.opposite();
 		}
+	}
+
+	public Player cachedWinner(GameState state, int depth) {
+		if (knownWinners.containsKey(state)) {
+			return knownWinners.get(state);
+		}
+		Player winner = winner(state, depth);
+		knownWinners.put(state, winner);
+		if (depth < 14) {
+			System.out.println(winner + " wins");
+			System.out.println(knownWinners.size() + " entries in cache");
+		}
+		return winner;
 	}
 }
